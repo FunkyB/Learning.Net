b80449ba57a4e1ec853fd08905c4af7ed5ca09cf
Aleksandr Nikulin <lethal.funky.b@gmail.com>
2022-02-09 10:42:41 +0300

    Add task #4 TernaryOperator

---
+++ 4. TernaryOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите 2 целых числа");
+Console.WriteLine("Введите первое число");
+bool intX = int.TryParse(Console.ReadLine(), out int x);
+if (!intX)
+{
+    Console.WriteLine("Введено не челое число");
+    return;
+}
+bool isEvenX = true ? x % 2 == 0 : x % 2 != 0;
+Console.WriteLine($"Четное число - {isEvenX}");
+if (!isEvenX) x++;
+Console.WriteLine("Введите второе число");
+bool intY = int.TryParse(Console.ReadLine(), out int y);
+if (!intY)
+{
+    Console.WriteLine("Введено не челое число");
+    return;
+}
+bool isEvenY = true ? y % 2 == 0 : y % 2 != 0;
+Console.WriteLine($"Четное число - {isEvenY}");
+if (!isEvenY) y++;
+string result = (x + y) % 4 == 0 ? $"Сумма значений {x} и {y} делится на 4" : $"Сумма значений {x} и { y} на 4 не делится";
+Console.WriteLine(result);
