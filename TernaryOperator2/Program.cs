d8813b5024bf9caf9b5dcff0532ac61e38597042
Aleksandr Nikulin <lethal.funky.b@gmail.com>
2022-02-11 00:02:51 +0300

    Add task #5 ternary operator 2

---
+++ TernaryOperator2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите 2 целых числа");
+Console.WriteLine("Введите первое число");
+bool intX = int.TryParse(Console.ReadLine(), out int x);
+if (!intX)
+{
+    Console.WriteLine("Введено не челое число");
+    return;
+}
+Console.WriteLine("Введите второе число");
+bool intY = int.TryParse(Console.ReadLine(), out int y);
+if (!intY)
+{
+    Console.WriteLine("Введено не челое число");
+    return;
+}
+(x >= y ? ref x : ref y) = 100;
+Console.WriteLine($"Итогвый результат: первое число {x}, второе число {y}");
