ea84f7e9f503a98a79ca412eee7b87ee0ac237a5
Aleksandr Nikulin <lethal.funky.b@gmail.com>
2022-02-21 20:09:37 +0300

    Updated solution of task #7

--- 7.LoopWhile2/Program.cs
+++ 7.LoopWhile2/Program.cs
@@ -1,21 +1,26 @@
 ﻿Console.WriteLine("Введите 2 целых числа");
 Console.WriteLine("Введите первое число");
-bool intX = int.TryParse(Console.ReadLine(), out int x);
-if (!intX)
+bool isValidX = int.TryParse(Console.ReadLine(), out int x);
+if (!isValidX)
 {
-    Console.WriteLine("Введено не челое число");
+    Console.WriteLine("Введено не целое число");
     return;
 }
 Console.WriteLine("Введите второе число");
-bool intY = int.TryParse(Console.ReadLine(), out int y);
-if (!intY)
+bool isValidY = int.TryParse(Console.ReadLine(), out int y);
+if (!isValidY)
 {
-    Console.WriteLine("Введено не челое число");
+    Console.WriteLine("Введено не целое число");
     return;
 }
 var start = Math.Min(x, y) + 1;
 var end = Math.Max(x, y);
 Console.WriteLine("Результат:");
+if (start == end)
+{
+    Console.WriteLine("Введеные числа равны, невозможно выполнить операцию");
+    return;
+}
 while (start < end)
 {
     if (start % 2 != 0)
