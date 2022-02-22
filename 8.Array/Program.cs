7fa57f8914dee8c7c1245f6cfdc310e6960a4e92
Aleksandr Nikulin <lethal.funky.b@gmail.com>
2022-02-22 20:43:44 +0300

    Added tasks # 8,9

---
+++ 8.Array/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] months = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+int monthsCounter = 0;
+Console.WriteLine("Введите название первого месяца года с заглавной буквы");
+while (monthsCounter < months.Length)
+{
+    string? input = Console.ReadLine();
+    if (input == months[monthsCounter])
+    {
+        monthsCounter++;
+    }    
+    else
+    {
+        bool ErrorInInputOfFiveMount = monthsCounter == 4;
+        if (ErrorInInputOfFiveMount)
+        {
+            Console.WriteLine("Ошибка в вводе значения пятого месяца, завершение цикла");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введено неверное значение, попробуйте снова");
+        }
+    }    
+}
+if (monthsCounter == months.Length)
+{
+    Console.WriteLine("Введены все 12 месяцев");
+}
