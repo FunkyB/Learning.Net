da19cc89676ea6c7786d7ac5f9abe8efa3647a84
Aleksandr Nikulin <lethal.funky.b@gmail.com>
2022-02-24 18:26:44 +0300

    Add task #11

---
+++ LoopFor/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("\n1");
+for (int index = 0; index <= 100; index++)
+{
+    Console.Write($"{index} ");
+}
+Console.WriteLine("\n2");
+for (int index = 1001; index <= 2500; index += 3)
+{
+    Console.Write($"{index} ");
+}
+Console.WriteLine("\n3");
+for (int index = 100; index >= 0; index -= 4)
+{
+    Console.Write($"{index} ");
+}
+Console.WriteLine("\n4");
+for (float index = 1; index <= 5.0; index += (float)0.2)
+{
+    Console.Write($"{Math.Round(index, 2)} ");
+}
