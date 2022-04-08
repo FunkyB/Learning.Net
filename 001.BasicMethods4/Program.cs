34cbe8cfdbb08264102267410f6ee457b15a0617
Aleksandr Nikulin <lethal.funky.b@gmail.com>
2022-04-08 15:59:59 +0300

    Add task

---
+++ 001.BasicMethods4/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Kata
+{
+    static void Main()
+    {
+        Digitize(35231);
+        //AbbrevName("Patrick Feenan");
+    }
+
+    public static string Digitize(long n)
+    {
+        string stringInput = Convert.ToString(n);
+        string[] test = stringInput.Split(stringInput.Length);
+        test.Reverse();
+
+        long[] LongNum = new long[test.Length];
+        for (int i = 0; i < test.Length; i++)
+        {
+            numberArray[i] = no.Substring(counter, 1)
+            LongNum[i] = test[i];
+        }
+        Console.WriteLine(LongNum.ToString());
+        //long l = test;
+
+        return "";
+    }
+}
