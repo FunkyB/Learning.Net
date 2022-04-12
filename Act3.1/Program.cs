5e5880590ff06a33cbd652a7a650ad7a6f6d142b
Aleksandr Nikulin <lethal.funky.b@gmail.com>
2022-04-12 16:16:35 +0300

    Add 3 tasks

---
+++ Act3.1/Program.cs
@@ -0,0 +1,38 @@
+﻿//В консоль вводятся два числа. В обоих числах есть одна общая цифра.
+//Выведите в консоль цифру, которая есть в обоих числах через пробел столько раз, сколько раз она встречается в первом числе.
+//Sample Input:
+//76652
+//63416
+//Sample Output:
+//6 6
+
+using System;
+
+public class MainClass
+{
+    //public static void Main()
+    //{
+    //    int firstNumber = int.Parse(Console.ReadLine());
+    //    int secondNumber = int.Parse(Console.ReadLine());
+
+    //    // Ваш код       
+
+
+    //}
+    public static void Main()
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        //int secondNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("\n");
+
+        for (int i = 0; i < firstNumber; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(j);
+            }
+            Console.Write("\n");
+        }
+    }
+}
--- Act3/Program.cs
+++ Act3/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая считает сумму всех простых чисел в определенном диапазоне.
+//Простое число - целое положительное число, имеющее ровно два различных натуральных делителя - единицу и самого себя.
+//Число 1 не является ни простым, ни составным числом, так как у него только один делитель - 1.
+//Sample Input:
+//-100
+//100
+//Sample Output:
+
+//Сумма простых чисел = 1060
+
+using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        int startValue = int.Parse(Console.ReadLine());
+        int endValue = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        for (int number = startValue; number < endValue; ++number)
+        {
+            if (number == 2)
+            {
+                sum += number;
+                continue;
+            }
+            if (number % 2 == 0 || number <= 1) continue;
+
+            bool isPrimeNumber = true;
+
+            for (int i = 3; i * i <= number; i += 2)
+            {
+                if (number % i == 0)
+                {
+                    isPrimeNumber = false;
+                    break;
+                }
+            }
+
+            if (isPrimeNumber)
+            {
+                sum += number;
+            }
+        }
+        Console.WriteLine($"Сумма простых чисел = {sum}");
+    }
+}
