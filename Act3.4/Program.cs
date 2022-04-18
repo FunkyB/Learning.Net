d0254969d669922bed3c69188618e46ba8c507b2
Aleksandr Nikulin <lethal.funky.b@gmail.com>
2022-04-18 13:05:18 +0300

    Add task 3.4

---
+++ Act3.4/Program.cs
@@ -0,0 +1,22 @@
+﻿//Факториалом натурального числа n называют произведение всех натуральных чисел от 1 до n включительно. Напишите программу для вычисления факториала введенного пользователем числа.
+//Sample Input:
+//10
+//Sample Output:
+//3628800
+
+using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+        int factorial = 1;
+
+        for (int i = 1; i <= num; i++)
+        {
+            factorial *= i;
+        }
+        Console.WriteLine(factorial);
+    }
+}
