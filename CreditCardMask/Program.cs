439b171a30cc99a73ed02c17d243a4cceaf9e9af
Aleksandr Nikulin <lethal.funky.b@gmail.com>
2022-04-20 00:12:13 +0300

    Add Maskify task

---
+++ CreditCardMask/Program.cs
@@ -0,0 +1,20 @@
+﻿public static class Kata
+{
+    static void Main()
+    {
+        Maskify("4556364607935616");
+        Maskify("1");
+        Maskify("11111");
+    }
+    
+    public static string Maskify(string cc)
+    {
+        const int NumberOfDigitsShowing = 4;
+        if (cc.Length > 4)
+        {
+            string visiblePart = cc.Remove(0, cc.Length - NumberOfDigitsShowing);
+            cc = new string('#', cc.Length - 4) + visiblePart;
+        }
+        return cc;
+    }
+}
