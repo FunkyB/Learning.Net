c84e28728f8ac80f5e032f2c55673b3434731c07
Aleksandr Nikulin <lethal.funky.b@gmail.com>
2022-04-21 16:01:14 +0300

    Temp commit Narcissistic numbers

---
+++ NarcissisticNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿public static class Kata
+{
+    static void Main()
+    {
+        IsNarcissistic(548834);
+        IsNarcissistic(1741725);
+        IsNarcissistic(4210818);
+        IsNarcissistic(9800817);
+        IsNarcissistic(9926315);
+    }
+
+    public static bool IsNarcissistic(long n)
+    {
+        int numberOfDigits = n.ToString().Length;
+        char [] intArray = n.ToString().ToCharArray();
+        int result = 0;
+        for (int i = 0; i < numberOfDigits; i++)
+        {
+            
+            result += Convert.ToInt32(intArray[i]) * numberOfDigits;
+
+            if (result == n)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
