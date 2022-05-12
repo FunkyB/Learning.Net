935d5445b437c1d71a7d9e20b2a7ab5de7604f88
Aleksandr Nikulin <lethal.funky.b@gmail.com>
2022-05-12 17:06:05 +0300

    add task

---
+++ Reverse words/Program.cs
@@ -0,0 +1,16 @@
+﻿public static class Kata
+{
+    static void Main()
+    {
+        ReverseWords("This is an example!");
+    }
+
+    public static void ReverseWords(string str)
+    {
+        List<string> result = str.Split(' ').ToList();
+        for (int i = 0; i < result.Count; i++)
+        {
+            result[i].Reverse();
+        }
+    }
+}
