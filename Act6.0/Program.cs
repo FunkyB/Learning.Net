12d595ac0101752deb6ec79374199c74459302ec
Aleksandr Nikulin <lethal.funky.b@gmail.com>
2022-05-26 14:07:14 +0300

    Update project structure and tasks

---
+++ Act6.0/Program.cs
@@ -0,0 +1,47 @@
+﻿//Метод GetTypeInfo должен возвращать строку с информацией о типе переданной в него переменной:
+//"Переменная является строкой"
+//"Переменная целочисленного типа"
+//"Переменная - число с плавающей точкой"
+//"Переменная является символом"
+
+//public static void Main()
+//{
+//    string str = Console.ReadLine();
+//    int a = int.Parse(str);
+//    double b = (double)a;
+//    char c = str[0];
+//    Console.WriteLine(GetTypeInfo(str));
+//    Console.WriteLine(GetTypeInfo(a));
+//    Console.WriteLine(GetTypeInfo(b));
+//    Console.WriteLine(GetTypeInfo(c));
+//}
+//Sample Input:
+//123
+//Sample Output:
+//Переменная является строкой
+//Переменная целочисленного типа
+//Переменная - число с плавающей точкой
+//Переменная является символом
+
+class Program
+
+{
+    public static void Main()
+    {
+        string str = Console.ReadLine();
+        int a = int.Parse(str);
+        double b = (double)a;
+        char c = str[0];
+        Console.WriteLine(GetTypeInfo(str));
+        Console.WriteLine(GetTypeInfo(a));
+        Console.WriteLine(GetTypeInfo(b));
+        Console.WriteLine(GetTypeInfo(c));
+    }
+    public static string GetTypeInfo(string input) => "Переменная является строкой";
+
+    public static string GetTypeInfo(int input) => "Переменная целочисленного типа";
+
+    public static string GetTypeInfo(double input) => "Переменная - число с плавающей точкой";
+
+    public static string GetTypeInfo(char input) => "Переменная является символом";
+}
---
+++ Act6.1/Program.cs
@@ -0,0 +1,31 @@
+﻿//Программа должна выводить в консоль строку, состоящую из amount количества символов symbol. Напишите метод Print.
+//public static void Main()
+//{
+//    string symbol = Console.ReadLine();
+//    string amount = Console.ReadLine();
+//    Print(symbol, amount, out string result);
+//    Console.WriteLine(result);
+//}
+//Sample Input:
+//A
+//5
+//Sample Output:
+//AAAAA
+
+class Program
+
+{
+    public static void Main()
+    {
+        string symbol = Console.ReadLine();
+        string amount = Console.ReadLine();
+        Print(symbol, amount, out string result);
+        Console.WriteLine(result);
+    }
+
+    private static string Print (string symbol, string amount, out string result)
+    {
+        return result = new string(Convert.ToChar(symbol), Convert.ToInt32(amount));
+    }
+
+}
